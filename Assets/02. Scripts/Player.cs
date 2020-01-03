b9dc87218d3afc2d491ef2b6107d8f01186cd2b1
kkwcathy <kkwcathy@daum.net>
2020-01-04 02:52:24 +0900

    base work

---
+++ Assets/02. Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] private float speed;
+
+	public List<GameObject> wayPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+	}
+
+	private void MovePoint()
+	{
+		
+	}
+
+	void Update()
+    {
+		transform.Translate(wayPoints[0].transform.position * Time.deltaTime * speed);
+		Debug.Log(transform.position);
+	}
+}
