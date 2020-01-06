478cbda709e49e0dcfda4a65f5b2d9f826a2f165
kkwcathy <kkwcathy@daum.net>
2020-01-06 22:07:56 +0900

    몬스터 생성 밑작업

---
+++ Assets/02. Scripts/EnemyGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+
+	[SerializeField] private GameObject enemyPrefab;
+
+	public Transform initPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		StartCoroutine(GenerateEnemy());
+    }
+
+	IEnumerator GenerateEnemy()
+	{
+		yield return new WaitForSeconds(3.0f); // 첫 생성
+
+		GameObject enemy = Instantiate(enemyPrefab, initPosition);
+	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
